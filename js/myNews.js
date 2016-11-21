2e267c1b37bb7cc377698732e9b5c4d5268f1c3e
manan1234 <794630336@qq.com>
2016-11-21 16:11:58 +0800

    '

--- js/myNews.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	$(".xxk div").on("touchend",function(){
-		var index=$(this).index();
-		$(".xxk div").removeClass("active");
-		$(this).addClass('active');
-		$("ul").css("display","none").eq(index).css("display","block");
-	})
-})
